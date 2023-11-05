547ef3b27b560937c2053996b318d87cce01e232
Zbreno <brenosoares9533@gmail.com>
2023-11-05 19:54:45 -0300

    add: repository

---
+++ delivery/repository/Rating.py
@@ -0,0 +1,42 @@
+from typing import Dict, Any, List
+from sqlalchemy.orm import Session
+from domain.data.sqlalchemy_models import Rating
+
+
+class RatingRepository:
+    
+    def __init__(self, sess:Session):
+        self.sess:Session = sess
+    
+    def insert_rating(self, rating: Rating) -> bool: 
+        try:
+            object_mapper = Rating(**rating)
+            self.sess.add(object_mapper)
+            self.sess.commit()
+        except: 
+            return False 
+        return True
+    
+    def update_rating(self, id:int, details:Dict[str, Any]) -> bool: 
+       try:
+             self.sess.query(Rating).filter(Rating.id == id).update(details)     
+             self.sess.commit() 
+           
+       except: 
+           return False 
+       return True
+   
+    def delete_rating(self, id:int) -> bool: 
+        try:
+           rating = self.sess.query(Rating).filter(Rating.id == id).delete()
+           self.sess.commit()
+          
+        except: 
+            return False 
+        return True
+    
+    def get_all_rating(self) -> List[Rating]:
+        return self.sess.query(Rating).all() 
+    
+    def get_rating(self, id:int) -> Rating: 
+        return self.sess.query(Rating).filter(Rating.id == id).one_or_none()
