2fb88890d9c70bef7b22458a6a33e046e1e2d6b3
Zbreno <brenosoares9533@gmail.com>
2023-11-05 21:32:17 -0300

    remove: configuration

--- app/configuration/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseSettings
-from datetime import date
-import os
-
-class FacultySettings(BaseSettings): 
-    application:str = 'Faculty Management System' 
-    webmaster:str = 'sjctrags@university.com'
-    created:date = '2021-11-10'
