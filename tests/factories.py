fad85b3d42107ce29d3fdb2b61be71b0721124c4
Zbreno <brenosoares9533@gmail.com>
2023-11-17 09:30:27 -0300

    add: category, address and product factory

--- tests/factories.py
+++ tests/factories.py
@@ -1,4 +1,5 @@
-from domain.data.sqlalchemy_models import Address, User, Ticket
+from domain.data.sqlalchemy_models import Address, User, Ticket, Category, Product
+
 from .test_common import Session
 import factory
 from uuid import uuid4
@@ -33,6 +34,8 @@ class AddressFactory(factory.alchemy.SQLAlchemyModelFactory):
     number = factory.Faker('building_number')
     complement = factory.Faker('secondary_address')
     reference_point = factory.Faker('sentence')
+    user = factory.SubFactory(UserFactory)
+    user_id = factory.SelfAttribute('user.id')
     
     
 class TicketFactory(factory.alchemy.SQLAlchemyModelFactory):
@@ -45,3 +48,23 @@ class TicketFactory(factory.alchemy.SQLAlchemyModelFactory):
     code = factory.Sequence(lambda n: str(uuid4()))
     description = factory.Faker('sentence')
     type = factory.Faker('random_element', elements=['company', 'client'])
+    
+class CategoryFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Category
+        sqlalchemy_session = Session
+    
+    id = factory.Sequence(lambda n: str(uuid4()))
+    name = factory.Faker('name')
+    
+
+class ProductFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Product
+        sqlalchemy_session = Session
+        
+    id = factory.Sequence(lambda n: str(uuid4()))
+    name = factory.Faker('name')
+    description = factory.Faker('sentence')
+    cost = factory.Faker('random_number', digits=2)
+    
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,7 @@
 import unittest
-from .factories import AddressFactory, UserFactory, TicketFactory
+from .factories import AddressFactory, UserFactory, TicketFactory, ProductFactory, CategoryFactory
 from .test_common import Session
-from domain.data.sqlalchemy_models import Address, User, Ticket
+from domain.data.sqlalchemy_models import Address, User, Ticket, Product, Category
 
 class TestUserModel(unittest.TestCase):
     def setUp(self):
@@ -17,24 +17,41 @@ class TestUserModel(unittest.TestCase):
         self.assertIsNotNone(retrieved_user)
         self.assertEqual(user.name, retrieved_user.name)
         
+    def test_create_product(self):
+        product = ProductFactory()
+
+        self.session.commit()
+
+        retrieved_product = self.session.get(Product, product.id)
+        self.assertIsNotNone(retrieved_product)
+        self.assertEqual(product.name, retrieved_product.name)
+        
+    def test_create_category(self):
+        category = CategoryFactory()
+
+        self.session.commit()
+
+        retrieved_category = self.session.get(Category, category.id)
+        self.assertIsNotNone(retrieved_category)
+        self.assertEqual(category.name, retrieved_category.name)
+        
     def test_create_ticket(self):
         ticket = TicketFactory()
 
         self.session.commit()
 
         retrieved_ticket = self.session.get(Ticket, ticket.id)
-        print(retrieved_ticket)
         self.assertIsNotNone(retrieved_ticket)
         self.assertEqual(ticket.code, retrieved_ticket.code)
         
-    # def test_create_address(self):
-    #     address = AddressFactory()
+    def test_create_address(self):
+        address = AddressFactory()
 
-    #     self.session.commit()
+        self.session.commit()
 
-    #     retrieved_address = self.session.get(Address, address.id)
-    #     self.assertIsNotNone(retrieved_address)
-    #     self.assertEqual(f"{address.city} - {address.district} / {address.street}",f"{retrieved_address.city} - {retrieved_address.district} / {retrieved_address.street}")
+        retrieved_address = self.session.get(Address, address.id)
+        self.assertIsNotNone(retrieved_address)
+        self.assertEqual(f"{address.city} - {address.district} / {address.street}",f"{retrieved_address.city} - {retrieved_address.district} / {retrieved_address.street}")
 
     def tearDown(self):
         self.session.rollback()
